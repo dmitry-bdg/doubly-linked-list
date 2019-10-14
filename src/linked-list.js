3949177b11eb9f691d20c1eb61928f84346e8783
Dmitry Balahonov <balahonov.dmitry@gmail.com>
2019-10-14 09:22:03 +0300

    feat: add constructor, append, head, tail, isEpmty

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,21 +1,53 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        let node = new Node(data);
+        if (this._tail != null) {
+            this._tail.next  = node;
+            node.prev = this._tail;
+            this._tail = node;
+        } else {
+            this._head = node;
+            this._tail = node;
+        }
+        this.length++;
+        return this;
+    }
 
-    head() {}
+    head() {
+        if (this._head) { 
+            return this._head.data;
+        } else {
+            return this._head;
+        }
+    }
 
-    tail() {}
+    tail() {
+        if (this._tail) {
+            return this._tail.data;
+        } else {
+            return this._tail;
+        }
+    }
 
     at(index) {}
 
     insertAt(index, data) {}
 
-    isEmpty() {}
+    isEmpty() {
+        return this._head == null ? true : false;
+    }
 
-    clear() {}
+    clear() {
+        
+    }
 
     deleteAt(index) {}
 
